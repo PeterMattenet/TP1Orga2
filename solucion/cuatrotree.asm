685a1f5e9cf064ae55fbf909451a9fcff018ea13
PeterMattenet <peter_matt@hotmail.com>
2016-08-27 18:28:26 -0300

    hola

--- solucion/cuatrotree.asm
+++ solucion/cuatrotree.asm
@@ -66,10 +66,53 @@ ct_delete:
 		push rbp
 		push r12
 
-		mov rbp, rsp
-		xor r12, r12
+		mov rbp, rsp  ;creo stack frame
+		sub rsp, 8    ;alineo a 16 la pila
+    mov r12, [rdi]  ;guardo el puntero a la raiz 
 
-        ret
+    .ciclo:
+
+      .hijo1:
+        cmp [r12 + offset_child1], NULL     ;comparo si hijo1 es null
+        je .hijo2                           ;si lo es, paso al siguiente
+        lea r12, [r12 + offset_child1]      ;pongo en r12 la direccion de donde esta el nuevo puntero a borrar
+        mov r12, [r12]                      ;pongo en r12 el puntero a borrar
+        jmp .ciclo
+
+      .hijo2:
+        cmp [r12 + offset_child2], NULL     ;comparo si hijo2 es null
+        je .hijo3                           ;si lo es, paso al siguiente
+        lea r12, [r12 + offset_child2]      ;pongo en r12 la direccion de donde esta el nuevo puntero a borrar
+        mov r12, [r12]                      ;pongo en r12 el puntero a borrar
+        jmp .ciclo
+
+      .hijo3:
+        cmp [r12 + offset_child3], NULL     ;comparo si hijo3 es null
+        je .hijo4                           ;si lo es, paso al siguiente
+        lea r12, [r12 + offset_child3]      ;pongo en r12 la direccion de donde esta el nuevo puntero a borrar
+        mov r12, [r12]                      ;pongo en r12 el puntero a borrar
+        jmp .ciclo
+
+      .hijo4:
+        cmp [r12 + offset_child4], NULL     ;comparo si hijo1 es null
+        je .estallameElNodo                 ;si lo es, vamos a eliminar el nodo
+        lea r12, [r12 + offset_child4]      ;pongo en r12 la direccion de donde esta el nuevo puntero a borrar
+        mov r12, [r12]                      ;pongo en r12 el puntero a borrar
+        jmp .ciclo
+
+      .estallameElNodo:
+        mov rdi, r12                        ;pongo en rdi la direccion del nodo a liberar
+        mov r12, [r12]                      ;pongo en r12 el puntero al padre del nodo a liberar
+        cmp r12, NULL                       ;comparo padre con null
+        je .fin                             ;si es null entonces termino todo
+        call free                           ;libero el nodo
+        jmp .ciclo                          ;repetimo'
+
+    .fin:
+      add rsp, 8
+      pop r12
+      pop rbp
+      ret
 
 ; ; =====================================
 ; ; void ct_aux_print(ctNode* node);
@@ -84,32 +127,134 @@ ct_print:
 ; =====================================
 ; ctIter* ctIter_new(ctTree* ct);
 ctIter_new:
+        push rbp
+        push r12
+        push rcx
+        mov rbp, rsp        ;creo nuevo stack frame
+        
+
+        mov r12, rdi        ;guardo el puntero al arbol
+        mov rdi, 21         ;cantidad de bytes para malloc
+        call malloc   
+
+        mov [rax + offset_tree], r12    ;ubico en el struct de malloc en los primeros 8 bytes la direccion del arbol parametro
+        mov qword [rax + offset_node], NULL      ;pongo en 0 el nodo actual
+        mov byte [rax + offset_current], NULL   ;pongo en 0 el current
+        mov dword [rax + offset_count], NULL    ;pongo en 0 el count
+
+        pop rcx
+        pop r12
+        pop rbp
         ret
 
 ; =====================================
 ; void ctIter_delete(ctIter* ctIt);
 ctIter_delete:
-        ret
+        jmp free
 
 ; =====================================
 ; void ctIter_first(ctIter* ctIt);
 ctIter_first:
+        push rbp
+        push r12
+        mov rbp, rsp
+        sub rsp, 8
+
+        mov r12, [rdi]      ;muevo a r12 el puntero al cuatree que deberia haber en los primeros 8 bytes del iterador al que rdi apunta
+        mov r12, [r12]      ;ubico en r12 el puntero que deberia estar en los primeros 8 bytes del cuatree 
+
+        mov [rdi + offset_node], r12
+
+        pop r12
+        pop rbp
         ret
 
 ; =====================================
 ; void ctIter_next(ctIter* ctIt);
 ctIter_next:
-        ret
+        push rbp
+        push r12
+        push rcx
+
+        mov rbp, rsp
+
+        mov r12, [rdi + offset_tree]      ;pongo en r12 el puntero al arbol
+        mov r12d, [r12 + offset_size]     ;ubico en los ultimos 4 bytes del registro el tamaño del arbol
+        cmp r12d, [rdi + offset_count]    ;comparo el tamaño con el numero de elemento del iterador
+        je .invalido                      ;jump al final porque el siguiente elemento no existe
+
+        mov cl, [rdi + offset_current]    ;tomo el numero de elemento del iterador
+        mov r12, [rdi + offset_node]      ;muevo a r12 el puntero al nodo sobre el que estoy iterando
+
+        lea rax, [r12 + offset_child2]    ; ubico en rax la direccion del segundo elemento del arreglo de punteros hijos
+        lea rax, [rax + cl*8]             ;le incremento segun el current
+
+        cmp qword [rax], NULL             ;si el puntero hijo es null nos movemos o al siguiente valor del nodo, o subimos
+        je .siguienteDelNodo
+
+        mov rax, [rax]                    ;ubico en rax el puntero al que apunta rax
+        .cicloMinimo:
+          mov rcx, [rax + offset_child1]  ;ubico en rcx el primero hijo del nodo en rax
+          cmp rcx, NULL
+          je .encontreElMinimo
+          mov rax, rcx
+          jmp .cicloMinimo
+
+        .encontreElMinimo:
+          mov [rdi + offset_node], rax    ;pongo en el struct del iterador pasado como parametro el nodo en cual freno la busqueda
+          mov byte [rdi + offset_current], 0  ;pongo el current en 0
+          inc [rdi + offset_count]
+          jmp .fin
+
+        .siguienteDelNodo:
+          mov cl, [r12 + offset_len]         ;pongo en cl el largo del nodo
+          cmp cl, [rdi + offset_current]     ;comparo el current con cl
+          jne .tomoElSiguiente
+
+          ;como garcha subo? esto en phython no pasa
+        
+
+        .tomoElSiguiente:
+          inc [rdi + offset_current]        ;incremento el valor de current
+          inc [rdi + offset_count]          ;incremento el valor del count
+          jmp .fin
+
+        .invalido:
+          mov qword [rdi + offset_node], NULL
+        
+        .fin:
+          pop rcx
+          pop r12
+          pop rbp
+          ret
 
 ; =====================================
 ; uint32_t ctIter_get(ctIter* ctIt);
 ctIter_get:
+        push rbp
+        push r12
+        xor r12, r12
+
+        mov r12, [rdi + offset_node]      ;obtengo la direccion del nodo en el iterador
+        lea r12, [r12 + offset_valor1]    ;muevo r12 hasta la direccion donde esta el primer valor
+        mov al, [rdi + offset_current]    ;muevo a al el numero de current del iterador
+
+        lea r12, [r12 + al*4 ]            ;pongo en r12 la direccion de donde deberia estar el valor indicado
+        mov eax, [r12]                    ;muevo a eax el valor dentro de dicha direccion
+
+        pop r12
+        pop rbp
         ret
 
 ; =====================================
 ; uint32_t ctIter_valid(ctIter* ctIt);
 ctIter_valid:
-        ret
+        mov eax, 0
+        cmp qword [rdi + offset_node], NULL
+        je .fin
+        mov eax, 1
+        .fin:
+          ret
 
 
 
